33240292a6be6d4c9e859e161de7c8d7c0967b6e
PSStefanov19 <PSStefanov19@codingburgas.bg>
2022-04-13 17:14:52 +0300

    Change User structure

--- pm.types/User.h
+++ pm.types/User.h
@@ -2,18 +2,32 @@
 
 #include "pch.h"
 
-namespace pm::types 
+namespace pm::types
 {
-	struct User
-	{
-		uint8_t id;
-		std::string username;
-		std::string passwordHash;
-		std::string firstName;
-		std::string lastName;
-		time_t dateOfCreation;
-		uint8_t idOfCreator;
-		time_t dateOfLastChange;
-		uint8_t idOfLastChangeUser;
-	};
+    struct User
+    {
+        uint8_t id;
+        std::string username;
+        std::string passwordHash;
+        std::string firstName;
+        std::string lastName;
+        std::string email;
+        time_t dateOfCreation;
+
+
+        User(uint8_t id,
+            std::string username,
+            std::string passwordHash,
+            std::string firstName,
+            std::string lastName,
+            std::string email,
+            time_t dateOfCreation
+            ) : id(id),
+            username(username),
+            passwordHash(passwordHash),
+            firstName(firstName),
+            lastName(lastName),
+            email(email),
+            dateOfCreation(dateOfCreation) {}
+    };
 }
