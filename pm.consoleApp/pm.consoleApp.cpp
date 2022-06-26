76112ab3f1c9bd5af6c6d635c7c9d477bebdd773
PSStefanov19 <PSStefanov19@codingburgas.bg>
2022-06-26 10:19:42 +0300

    Create Business logic layer

--- pm.consoleApp/pm.consoleApp.cpp
+++ pm.consoleApp/pm.consoleApp.cpp
@@ -1,16 +1,6 @@
-#include "../pm.dal/UserManager.h"
-#include "../pm.dal/TeamManager.h"
 #include <iostream>
 
 int main()
 {
-	pm::dal::UserManager& uMan = pm::dal::UserManager::getInstance();
-	pm::dal::TeamManager& tMan = pm::dal::TeamManager::getInstance();
-	std::filesystem::create_directories("../data");
-	if (!std::filesystem::exists("../data/teams.csv"))
-	{
-		tMan.createDB();
-	}
-	int ids[] = { 1, 4, 5 };
-	tMan.createTeam("Team1", ids);
+
 }
